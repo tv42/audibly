be898c373fa3ad7d783920983aab0c96b3305799
Tommi Virtanen <tv@eagain.net>
2014-08-27 00:28:52 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# `audibly` -- Audibly report command status
+
+Run it like `audibly COMMAND [ARGS..]`, and `audibly` will report the
+success or error of `COMMAND` with two different sounds.
+
+It currently uses [eSpeak](http://espeak.sourceforge.net/) as the
+speech synthesizer, and requires it to be installed. This may change
+later.
+
+``` console
+# this will say "success: echo"
+$ audibly echo hello, world
+
+# this will say "success: greeting"
+$ audibly -name=greeting -- echo hello, world
+
+# this will say "hurrah"
+$ audibly -success=hurrah -- echo hello, world
+
+# this will say "oh noes"
+$ audibly -failure="oh noes" false
+
+# this will say "status 42"
+$ audibly -failure='status {{.Status}}' sh -c 'exit 42'
+```
+
+You can also use templates to generate the message spoken.
+The following fields are defined:
+
+- `Name`: value of the `-name=` flag, or the basename of the command
+- `Status`: exit status of the command
+- `Cmd`: the [os/exec Cmd value](http://golang.org/pkg/os/exec/#Cmd)
+
+The template mechanism is documented at
+http://golang.org/pkg/text/template/
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+	"text/template"
+)
+
+var (
+	name    = flag.String("name", "", "how to identify this run (default: basename of command)")
+	success = flag.String("success", "success: {{.Name}}", "say this message on success")
+	failure = flag.String("failure", "failure: {{.Name}}", "say this message on failure")
+)
+
+type Info struct {
+	name string
+	Cmd  *exec.Cmd
+}
+
+func (i *Info) Name() string {
+	if *name != "" {
+		return *name
+	}
+	return filepath.Base(i.Cmd.Args[0])
+}
+
+func (i *Info) Status() int {
+	if i.Cmd.ProcessState.Success() {
+		return 0
+	}
+	if status, ok := i.Cmd.ProcessState.Sys().(syscall.WaitStatus); ok {
+		if status.Exited() {
+			return status.ExitStatus()
+		}
+		if status.Signaled() {
+			return 128 + int(status.Signal())
+		}
+	}
+	// uhh.. something. 1 is too common, and 2 is typical usage error.
+	return 3
+}
+
+func run(args []string) (*Info, error) {
+	i := &Info{
+		Cmd: exec.Command(args[0], args[1:]...),
+	}
+	i.Cmd.Stdin = os.Stdin
+	i.Cmd.Stdout = os.Stdout
+	i.Cmd.Stderr = os.Stderr
+
+	if err := i.Cmd.Run(); err != nil {
+		switch err.(type) {
+		case *exec.ExitError:
+			// we don't consider runs that actually start the child as failures
+		default:
+			return i, err
+		}
+	}
+	return i, nil
+}
+
+func report(i *Info) error {
+	msg, err := format(i)
+	if err != nil {
+		return err
+	}
+	if err := speak(msg); err != nil {
+		return err
+	}
+	return nil
+}
+
+func format(i *Info) (string, error) {
+	var tmpl string
+	if i.Cmd.ProcessState.Success() {
+		tmpl = *success
+	} else {
+		tmpl = *failure
+	}
+
+	t, err := template.New("speech").Parse(tmpl)
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	if err := t.Execute(&buf, i); err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
+
+func speak(msg string) error {
+	cmd := exec.Command("espeak", "--", msg)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+var prog = filepath.Base(os.Args[0])
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "Usage of %s:\n", prog)
+	fmt.Fprintf(os.Stderr, "  %s [OPTS] [--] COMMAND [ARGS..]\n", prog)
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Fprintf(os.Stderr, "Options:\n")
+	flag.PrintDefaults()
+}
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix(prog + ": ")
+
+	flag.Usage = usage
+	flag.Parse()
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	i, err := run(flag.Args())
+	if err != nil {
+		log.Fatalf("starting command: %v", err)
+	}
+	if err := report(i); err != nil {
+		log.Fatalf("espeak: %v", err)
+	}
+	os.Exit(i.Status())
+}
